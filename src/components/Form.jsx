4a0201ebfdff669c35ef694e6b26e644c9f1f37e
Juan Galarce <j6alarce@gmail.com>
2022-07-29 20:23:19 -0400

    Se leen los camposn de los input de form, se añade un state a formulario

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import { useState } from "react";
 
 const Form = () => {
-  return (
-    <div>Form</div>
-  )
-}
+  const [search, setSearch] = useState({
+    artist: "",
+    song: "",
+  });
 
-export default Form
+  const handleSubmit = () => {};
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <legend>Busca por artistas y cancion</legend>
+      <div className="form-grid">
+        <div>
+          <label>Artista</label>
+          <input
+            type="text"
+            name="artist"
+            placeholder="Nombre Artista"
+            value={search.artist}
+            onChange={(e) =>
+              setSearch({
+                ...search,
+                [e.target.name]: e.target.value,
+              })
+            }
+          />
+        </div>
+        <div>
+          <label>Cancion</label>
+          <input
+            type="text"
+            name="song"
+            placeholder="Nombre Canción"
+            value={search.song}
+            onChange={(e) =>
+              setSearch({
+                ...search,
+                [e.target.name]: e.target.value,
+              })
+            }
+          />
+        </div>
+        <input type="submit" value="buscar" />
+      </div>
+    </form>
+  );
+};
+
+export default Form;
